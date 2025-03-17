49a8e0afc459dd6954c9d0b935c290fc2940fd2f
christiancompiles <christiancompiles@gmail.com>
2025-03-17 11:30:37 -0700

    Using Sage for discrete log.

--- imp_secFleo.py
+++ imp_secFleo.py
@@ -3,12 +3,15 @@ from charm.toolbox.eccurve import prime192v1
 from secrets import randbelow
 from random import randrange
 from math import isqrt
+from sage.all import discrete_log, GF
+
 RED = '\033[31m'
 RESET = '\033[0m'
 
+#import gmpy2
 # from functools import reduce
 # import gmpy2
-# from gmpy2 import mpz, is_prime, mpz_random
+# , is_prime, mpz_random
 # #import math
 # #from typing import Optional
 # from sympy import isprime, nextprime, primitive_root, randprime
@@ -16,122 +19,144 @@ RESET = '\033[0m'
 # import hashlib
 # import time
 
-def discrete_log_bound(target, gen, bounds):
-    """
-    Find the discrete log of a under base g within bounds using Pollard's Kangaroo algorithm
+# def discrete_log_bound(target, gen, bounds):
+#     """
+#     Find the discrete log of a under base g within bounds using Pollard's Kangaroo algorithm
 
-    :param a: Target element
-    :param g: Base element
-    :param bounds: Bounds for discrete log search
-    :return: Discrete log of a under base g
-    """
-    a = int(target)
-    g = int(gen)
-    width = bounds[1] - bounds[0]
-    if width < 1000:
-        return discrete_log_bound_brute(a, g, bounds)
+#     :param a: Target element
+#     :param g: Base element
+#     :param bounds: Bounds for discrete log search
+#     :return: Discrete log of a under base g
+#     """
+#     a = int(target)
+#     g = int(gen)
+#     width = bounds[1] - bounds[0]
+#     if width < 1000:
+#         return discrete_log_bound_brute(a, g, bounds)
 
-    lb = bounds[0]
-    ub = bounds[1]
+#     lb = bounds[0]
+#     ub = bounds[1]
 
-    N = isqrt(width) + 1
+#     N = isqrt(width) + 1
 
-    M = {}
-    for iterations in range(10):
-        # random walk function setup
-        k = 0
-        while 2 ** k < N:
-            r = randrange(1, N)
-            M[k] = (r, r * g)
-            k += 1
+#     M = {}
+#     for iterations in range(10):
+#         # random walk function setup
+#         k = 0
+#         while 2 ** k < N:
+#             #print("here 1")
+#             r = randrange(1, N)
+#             M[k] = (r, r * g)
+#             k += 1
 
-        # first random walk
-        H = ub * g
-        c = ub
-        for i in range(N):
-            r, e = M[hash(int(H)) % k]
-            H = H + e
-            c += r
+#         # first random walk
+#         H = ub * g
+#         c = ub
+#         for i in range(N):
+#             r, e = M[hash(int(H)) % k]
+#             H = H + e
+#             c += r
+#             #print("here 2")
 
-        ori = H
+#         ori = H
 
-        # second random walk
-        H = a
-        d = 0
-        while c - d >= lb:
-            if ub > c - d and H == ori:
-                return c - d
-            r, e = M[hash(H) % k]
-            H = H + e
-            d += r
+#         # second random walk
+#         H = a
+#         d = 0
+#         while c - d >= lb:
+#             if ub > c - d and H == ori:
+#                 return c - d
+#             r, e = M[hash(H) % k]
+#             H = H + e
+#             d += r
 
-    return discrete_log_bound_brute(a, g, bounds)
+#     return discrete_log_bound_brute(a, g, bounds)
 
+# def discrete_log_bound_brute(a, g, bounds):
+#     """
+#     Find the discrete log of a under base g within bounds using brute force
 
-def discrete_log_bound_brute(a, g, bounds):
-    """
-    Find the discrete log of a under base g within bounds using brute force
-
-    :param a: Target element
-    :param g: Base element
-    :param bounds: Bounds for discrete log search
-    :return: Discrete log of a under base g
-    """
-    cul = bounds[0] * g
-    for i in range(bounds[1] - bounds[0] + 1):
-        if cul == a:
-            ans = i + bounds[0]
-            return ans
-        cul = (cul + g)
-    raise Exception(f"Discrete log for {a} under base {g} not found in bounds ({bounds[0]}, {bounds[1]})")
+#     :param a: Target element
+#     :param g: Base element
+#     :param bounds: Bounds for discrete log search
+#     :return: Discrete log of a under base g
+#     """
+#     cul = bounds[0] * g
+#     for i in range(bounds[1] - bounds[0] + 1):
+#         if cul == a:
+#             ans = i + bounds[0]
+#             return ans
+#         cul = (cul + g)
+#     raise Exception(f"Discrete log for {a} under base {g} not found in bounds ({bounds[0]}, {bounds[1]})")
 
 if __name__ == "__main__":
 
     group = ECGroup(prime192v1)
-    q = group.order() # get the order. 5
+    q = 5 #group.order() # get the order. or use 5
 
     print(f'\norder: {q}\n')
-    g = group.random() # random generator. 4
+    g = 4 # group.random() # random generator. or use 4
     print(f'generator: {g}\n')
 
-    n_C = 3 # number of clients
+    n_C = 3 # number of clients (each client is in its own orbit)
     data = [[1] for _ in range(n_C)]
-    print(f'Data: {data}\n')
+    print(f'data: {data}\n')
 
     # step a
-    private_nums = [max(1, randbelow(int(q))) for _ in range(n_C)] # step a
+    private_nums = [randbelow(int(q)) for _ in range(n_C)] # step a
     print(f'private_nums: {private_nums}\n')
 
     # step b
-
     public_keys = [g**p_n for p_n in private_nums] # not using these values since using reduced equation
 
     # step c
-    gy = []
-    yi = []
+    gy_original = [] # shared keys using original method
+    gy_reduced = [] # shared keys using reduced method   
+    yi = [] # powers of shared key base 
 
     for sat_num in range(n_C):
         left = 0
         right = 0
         for j in range(n_C):
             if j < sat_num:
-                left  += private_nums[j]
+                left += private_nums[j]
             elif j > sat_num:
-                right  += private_nums[j]
+                right += private_nums[j]
       
         my_sum = left-right
         yi.append(my_sum)
         print(f'{left} - {right} =\n{my_sum}')
         gyi = g**my_sum
-        print(f'gyi: {gyi}\n')
-        gy.append(gyi)
+        print(f'gyi_reduced: {gyi}\n')
+        gy_reduced.append(gyi)
+
+    print(f'g^y: {gy_reduced}\n')
+
+    for sat_num in range(n_C):
+        numerator = 1
+        denominator = 1
+        for j in range(n_C):
+            if j < sat_num:
+                numerator *= g**private_nums[j]
+            elif j > sat_num:
+                denominator *= g**private_nums[j]
+
+        division = numerator / denominator
+        print(f'{numerator} / {denominator} =\n{division}')
+        
+        gy_original.append(division)
+        print(f'gyi_original: {gy_original[sat_num]}\n')
     
-    print(f'g^y: {gy}\n')
+    # compare original vs reduced:
+    assert gy_original == gy_reduced, "should have same shared key derivation"
                 
     # Step d
     secret_keys = [randbelow(int(q)) for _ in range(n_C)]#[max(1,randbelow(int(q))) for _ in range(n_C)]
-
     print(f'secret keys: {secret_keys}\n')
+
+    prop_1_check = sum([private_nums[i]*yi[i] for i in range(n_C)] )
+
+    assert prop_1_check == 0, "according to prop_1, should be 0"
 
     partial_agg_key = []
     for i in range(n_C):
@@ -152,10 +177,7 @@ if __name__ == "__main__":
 
     print(f'check_agg: {check_agg}')
 
-    if (aggregation_key == check_agg):
-        print('Aggregation key is correct')
-    else:
-        print(f'{RED}Aggregation key is incorrect.{RESET}')
+    assert aggregation_key == check_agg, "aggregation key is incorrect"
 
     # Step 2
 
@@ -171,11 +193,16 @@ if __name__ == "__main__":
     agg_model_param: int
 
     exp_sum = sum(cipher_data)
+    print(f'exp_sum: {exp_sum}')
 
     agg_model = g**exp_sum
     print(f'agg. model: {agg_model}')
     print(f'num digits: {len(str(agg_model))}')
 
-    result = discrete_log_bound(agg_model, g, (0,10000000000000000))
+    # width1 = 10000000000000000000
+    # width2 = 100000000000000000
+
+    # print(f'width: {width1-width2}')
+    result = discrete_log(GF(q)(agg_model), GF(q)(g)) #discrete_log_bound(16, 2, (0, 10))
 
     print(f'result: {result}')
